10df3032068e3f6b66bfb48c3e67707605606258
ahmed-fawzy-hub <af348565@gmail.com>
2023-12-29 20:09:43 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mydesignpatterns/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.mydesignpatterns
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val factory=HotDrinkFactory()
+        val myDrink=factory.create("Cappucchino")
+        myDrink.drink()
+    }
+}
